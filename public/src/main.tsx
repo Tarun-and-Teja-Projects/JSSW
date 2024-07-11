9a9832fcbcef358e582b236aa67ae52a4c3afccb
Dasari PrabhuTeja <118032158+prabhutejad@users.noreply.github.com>
2024-07-12 02:34:32 +0530

    New funtionality (#28)
    * JSSW added
    
    * JSSW added
    
    * JSSW added
    
    * Table and auth added
    
    * Table and auth added
    
    * test
    
    ---------
    
    Signed-off-by: Dasari PrabhuTeja <118032158+prabhutejad@users.noreply.github.com>

--- public/src/main.tsx
+++ public/src/main.tsx
@@ -6,13 +6,16 @@ import {  MantineProvider } from '@mantine/core';
 import '@mantine/core/styles.css';
 import { Provider } from 'react-redux'
 import { store } from './slices/store.ts';
+import { Notifications } from '@mantine/notifications';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <MantineProvider>
+    <Notifications >
      <Provider store={store}>
     <App />
     </Provider>
+    </Notifications>
     </MantineProvider>
   </React.StrictMode>,
 )
