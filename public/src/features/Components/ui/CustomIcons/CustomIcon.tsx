37c327d923461b062656b3117b00dd3dc81a9bc8
prabhutejad <prabhuteja.iot@gmail.com>
2024-09-23 10:35:02 +0530

    test

--- public/src/features/Components/ui/CustomIcons/CustomIcon.tsx
+++ public/src/features/Components/ui/CustomIcons/CustomIcon.tsx
@@ -19,30 +19,37 @@ const CustomIcon: React.FC<Props> = ({ label, type, onClick }) => {
         setIsHovered(false);
     };
 
+    const customProps = {
+        onMouseEnter: handleMouseEnter,
+        onMouseLeave: handleMouseLeave,
+        onClick,
+        style: { cursor: 'pointer' }
+    };
+
     const renderIcon = () => {
         switch (type) {
             case "edit":
                 return (
-                    <CustomTooltip label={label} children={<IconEdit
-                        onMouseEnter={handleMouseEnter}
-                        onMouseLeave={handleMouseLeave}
-                        style={{ color: isHovered ? "var(--mantine-color-blue-8)" : "#D3D3D3", cursor: 'pointer' }}
-                        onClick={onClick}
-                    />} />
+                    <CustomTooltip label={label}>
+                        <IconEdit
+                            {...customProps}
+                            style={{ color: isHovered ? "var(--mantine-color-blue-8)" : "#D3D3D3", ...customProps.style }}
+                        />
+                    </CustomTooltip>
                 );
 
             case "delete":
                 return (
-                    <CustomTooltip label={label} children={<IconTrash
-                        onMouseEnter={handleMouseEnter}
-                        onMouseLeave={handleMouseLeave}
-                        style={{ color: isHovered ? "var(--mantine-color-red-8)" : "#D3D3D3", cursor: 'pointer' }}
-                        onClick={onClick}
-                    />} />
+                    <CustomTooltip label={label}>
+                        <IconTrash
+                            {...customProps}
+                            style={{ color: isHovered ? "var(--mantine-color-red-8)" : "#D3D3D3", ...customProps.style }}
+                        />
+                    </CustomTooltip>
                 );
 
             default:
-                return null; 
+                return null;
         }
     };
 
